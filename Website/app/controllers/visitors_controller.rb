e66d514a3e24fe79fc3d0d07ae99220dbb4fc590
chenyu <believe.chenyu@gmail.com>
2014-12-29 15:11:49 +0800

    测试html注入攻击，sanitize

--- Website/app/controllers/visitors_controller.rb
+++ Website/app/controllers/visitors_controller.rb
@@ -1,2 +1,7 @@
 class VisitorsController < ApplicationController
+	def index
+		@html = '<a href="http://www.baidu.com">baidu</a><br/>'+
+			'<script>alert("233");</script>'+
+			'<img src="http://www.baidu.com/img/baidu_jgylogo3.gif?v=19576207.gif"/>'
+	end
 end
